47a37b186a81d62b5b8e182ce73a762fc5621255
rustamovjavohir <rustamovj366@gmail.com>
2022-05-18 17:20:20 +0500

    unit test commit

--- EcommerceSHOP/yasg.py
+++ EcommerceSHOP/yasg.py
@@ -15,7 +15,7 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
