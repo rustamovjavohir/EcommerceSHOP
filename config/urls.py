1893112009923957d1d6b5a18388e66b1d64c949
rustamovjavohir <rustamovj366@gmail.com>
2023-01-07 13:19:12 +0500

    change config

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""EcommerceSHOP URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
